aefff59201f574f0190d5ea61ba822d3889331b0
mdabarik <mdabarik19@gmail.com>
2023-07-28 20:38:25 +0600

    prereq added

---
+++ class-02.js
@@ -0,0 +1,9 @@
+/*
+1. number
+2. integer: 2, 5, 7
+3. float: decimal: 2.5, 13.67, 32.69
+4. operation: addition, substraction, multiplication, division, modulus(remainder)
+5. percent
+6. average
+7. comparison
+*/
